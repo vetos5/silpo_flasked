9140d9c999af8813ec09e99655ccf73496586cc2
Vitalii Kovtun <VITALII.KOVTUN@lnu.edu.ua>
2024-04-13 22:23:56 +0300

    Shopping cart added

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:silpo1@localhost/silpo'
 db = SQLAlchemy(app)
+
+shopping_cart = []
 
 
 class Product(db.Model):
@@ -31,6 +33,26 @@ def index():
     products = Product.query.all()
     return render_template('index.html', products=products)
 
+
+@app.route('/add_to_cart', methods=['POST'])
+def add_to_cart():
+    product_id = request.form['product_id']
+    product = Product.query.get(product_id)
+    if product:
+        cart_item = next((item for item in shopping_cart if item['id'] == product.id), None)
+        if cart_item:
+            cart_item['quantity'] += 1
+        else:
+            shopping_cart.append({'id': product.id, 'name': product.name, 'price': product.price, 'quantity': 1})
+    return redirect(url_for('index'))
+
+
+@app.route('/cart')
+def cart():
+    total = sum(item['price'] * item['quantity'] for item in shopping_cart)
+    total = format(total, '.2f')
+    return render_template('cart.html', cart=shopping_cart, total=total)
+
+
 if __name__ == '__main__':
     app.run()
-
